a0bbd593f8510e1e35f625e6e8437306419336d6
pft\\Ruby_Feng <Ruby_Feng@PerfectCorp.com>
2020-03-09 16:46:28 +0800

    Updates

---
+++ precache-manifest.b0a01ad5c430e7da60afaa4a858f064f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e63ca04f0a550d9823b84f2ad0fa45",
+    "url": "/chatroom/index.html"
+  },
+  {
+    "revision": "a682f96e2b176424a68a",
+    "url": "/chatroom/static/css/main.ed79309f.chunk.css"
+  },
+  {
+    "revision": "179b425a8594c991632b",
+    "url": "/chatroom/static/js/2.b9438d03.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chatroom/static/js/2.b9438d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a682f96e2b176424a68a",
+    "url": "/chatroom/static/js/main.f0937002.chunk.js"
+  },
+  {
+    "revision": "5cc0b3f4565f113efae1",
+    "url": "/chatroom/static/js/runtime-main.1a7a90db.js"
+  },
+  {
+    "revision": "18fad0a3753429a686cbb402c8de4863",
+    "url": "/chatroom/static/media/logo.18fad0a3.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chatroom/static/media/logo.5d5d9eef.svg"
+  }
+]);
